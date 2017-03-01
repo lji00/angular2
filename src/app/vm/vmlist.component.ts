5f3a62d45e4058a769c4b2f0dd1bb6132f47521e
Linghong Ji <lji@akamai.com>
2017-03-01 09:48:44 -0500

    add vm service and mock data

--- src/app/vm/vmlist.component.ts
+++ src/app/vm/vmlist.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import {VM} from './vm';
+import {VMService} from './vm.service';
 @Component({
     selector: 'vm',
-    templateUrl: './vmlist.component.html'
+    templateUrl: './vmlist.component.html',
+    providers: [VMService]
     //styleUrls: ['./dashboard.scss']
 
 })
-export class VMListComponent { }
+export class VMListComponent implements OnInit {
+    vms:VM[];
+    selectedVM:VM;
+
+    constructor(private vmService:VMService) {
+    }
+
+    ngOnInit():void {
+        this.vmService.getVMs().then(vmlist => {
+            this.vms = vmlist;
+        });
+    }
+
+    onSelect(vm:VM):void {
+        this.selectedVM = vm;
+    }
+}
