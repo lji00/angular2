020fc3d6a0d32c56606147195434caca56235789
Linghong Ji <lji@akamai.com>
2017-03-01 09:49:24 -0500

    add vm service and mock data

---
+++ src/app/mock/vms-mock.ts
@@ -0,0 +1,10 @@
+import {VM} from '../vm/vm';
+export const VMS:VM[] = [
+    //new VM(11, 'CentralOS', 'test'),
+    //new VM(12, 'Windows', 'test'),
+    //new VM(13, 'OSX', 'test')
+    {id: 11, name:'CentralOS', desc: 'test'},
+    {id: 12, name: 'Windows', desc: 'test'},
+    {id: 13, name: 'OSX', desc: 'test'}
+
+]
