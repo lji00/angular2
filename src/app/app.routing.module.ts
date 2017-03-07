08c72e73c52b46fa4a90fc55bc408c15753c042a
Linghong Ji <lji@akamai.com>
2017-03-07 17:36:18 -0500

    add auth module

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from './auth/auth-guard.service';
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: '/dashboard' }
+    { path: '', pathMatch: 'full', redirectTo: '/dashboard' ,    canActivate: [AuthGuard],
+    }
 ];
 
 @NgModule({
