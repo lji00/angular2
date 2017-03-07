14c6a67ab7323e314cdf496f4cdc43520cd6b3f4
Linghong Ji <lji@akamai.com>
2017-03-07 17:36:04 -0500

    add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppComponent } from './app.component';
 import {DashboardModule} from './dashboard/dashboard.module';
 import  {VMModule} from './vm/vm.module';
 import {AppRoutingModule}  from './app.routing.module';
+import {AuthModule} from './auth/auth.module';
+import { DialogService }           from './dialog.service';
 @NgModule({
     imports: [
-        BrowserModule, DashboardModule, VMModule, AppRoutingModule, NgbModule.forRoot()
+        BrowserModule, DashboardModule, VMModule, AppRoutingModule,
+        AuthModule,
+        NgbModule.forRoot()
     ],
     declarations: [
         AppComponent
+    ],
+    providers: [
+        DialogService
     ],
     bootstrap: [AppComponent]
 })
