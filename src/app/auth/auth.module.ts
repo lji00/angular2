19d37c654fd4ff72de5407787317b1291d7aee8d
Linghong Ji <lji@akamai.com>
2017-03-07 17:36:30 -0500

    add auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { LoginComponent } from './login.component';
+import {LoginRoutingModule} from './login-routing.module';
+import { CanDeactivateGuard }      from './can-deactivate-guard.service';
+import {AuthGuard} from './auth-guard.service';
+import {AuthService} from './auth.service';
+@NgModule({
+    imports: [
+        BrowserModule, LoginRoutingModule
+    ],
+    declarations: [
+        LoginComponent
+    ],
+    providers: [
+        AuthGuard,
+        AuthService,
+        CanDeactivateGuard
+    ]
+})
+export class AuthModule { }
