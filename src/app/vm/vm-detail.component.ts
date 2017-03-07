a90f5e7d18922202a33762623b3c5e3205065063
Linghong Ji <lji@akamai.com>
2017-03-07 17:37:41 -0500

    add auth module

--- src/app/vm/vm-detail.component.ts
+++ src/app/vm/vm-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import {VMService }  from './vm.service';
 import {VM} from './vm';
+import {DialogService} from '../dialog.service';
 @Component({
     selector: 'vm-detail',
     templateUrl: './vm-detail.component.html',
@@ -10,11 +11,14 @@ import {VM} from './vm';
 })
 
 
-
-
 export class VMDetailComponent implements OnInit {
     vm:VM;
-    constructor(private vmService:VMService, private router:Router, private route:ActivatedRoute) {}
+    orgVm:VM;
+
+    constructor(private vmService:VMService, private router:Router, private route:ActivatedRoute,
+                private dialogService:DialogService) {
+    }
+
     ngOnInit() {
         this.route.params
             // (+) converts string 'id' to a number
@@ -25,10 +29,30 @@ export class VMDetailComponent implements OnInit {
 
     gotoListView() {
         let vmId = this.vm ? this.vm.id : null;
-        this.router.navigate(['../',  { id: vmId}], {relativeTo: this.route });
+        this.router.navigate(['../', {id: vmId}], {relativeTo: this.route});
         //absolute route
         //this.router.navigate(['/vms', {id: vmId}]);
 
     }
+
+    save() {
+        //make rest call to save the vm
+        this.gotoListView();
+    }
+
+    cancel() {
+        this.gotoListView();
+    }
+
+    canDeactivate():Promise<boolean> | boolean {
+        // Allow synchronous navigation (`true`) if no crisis or the crisis is unchanged
+        if (!this.vm || this.vm === this.orgVm) {
+            return true;
+        }
+        // Otherwise ask the user with the dialog service and return its
+        // promise which resolves to true or false when the user decides
+        return this.dialogService.confirm('Discard changes?');
+    }
+
 
 }
--- src/app/vm/vm.routing.module.ts
+++ src/app/vm/vm.routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {VMListComponent} from './vmlist.component';
 import {VMDetailComponent} from './vm-detail.component';
+import { CanDeactivateGuard }    from '../auth/can-deactivate-guard.service';
+
 const vmRoutes: Routes = [
     { path: 'vms',  component: VMListComponent },
     //the detail router has to be at same level of list router if
     // we want detail view replacing the list view not added inside the list view,
-    {path:'vms/:id', component:VMDetailComponent}
+    {path:'vms/:id', component:VMDetailComponent,  canDeactivate: [CanDeactivateGuard]
+    }
 ];
 @NgModule({
     imports: [
